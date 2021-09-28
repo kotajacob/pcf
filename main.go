2f5c70ded3bf4308fc33b68499ca640f05f0a94c
Dakota Walsh <kota@nilsu.org>
2021-09-28 16:20:48 +1300

    fix multiconnection bug

--- main.go
+++ main.go
@@ -16,6 +16,73 @@ import (
 	"github.com/jlaffaye/ftp"
 )
 
+// calculate and print the hash
+func hash(f *os.File) string {
+	if _, err := f.Seek(0, 0); err != nil {
+		fmt.Fprintf(os.Stderr, "pcf: failed to read: %v\n", err)
+		os.Exit(1)
+	}
+	h := sha1.New()
+	if _, err := io.Copy(h, f); err != nil {
+		fmt.Fprintf(os.Stderr, "pcf: failed to hash: %v\n", err)
+	}
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+func main() {
+	addr := os.Getenv("PCFSERVER")
+	if addr == "" {
+		fmt.Println("pcf: you must set the PCFSERVER environment variable!")
+		os.Exit(1)
+	}
+
+	// parse the ftpURL
+	ftpURL, err := url.Parse(addr)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "pcf: url configuration wrong: %v\n", err)
+	}
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		// use stdin data
+		c := login(ftpURL)
+		store(ftpURL, os.Stdin, c, "file")
+		exit(c)
+		h := hash(os.Stdin)
+		webURL := *ftpURL
+		webURL.Host = ftpURL.Hostname()
+		webURL.Path = h
+		fmt.Println(&webURL)
+	} else {
+		// loop through and use all arguments
+		connection := login(ftpURL)
+		for _, arg := range args {
+			file, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "pcf: open: %v\n", err)
+				continue
+			}
+			defer file.Close()
+
+			if _, err := file.Seek(0, 0); err != nil {
+				fmt.Fprintf(os.Stderr, "pcf: failed to read: %v\n", err)
+				os.Exit(1)
+			}
+			store(ftpURL, file, connection, filepath.Base(arg))
+
+			// calculate the hash
+			h := hash(file)
+
+			// print the URL
+			webURL := *ftpURL
+			webURL.Host = ftpURL.Hostname()
+			webURL.Path = h + filepath.Ext(arg)
+			fmt.Println(&webURL)
+		}
+		exit(connection)
+	}
+}
+
 // create the ftp connection
 func login(u *url.URL) *ftp.ServerConn {
 	c, err := ftp.Dial(u.Host, ftp.DialWithTimeout(10*time.Second))
@@ -42,71 +109,5 @@ func exit(c *ftp.ServerConn) {
 	err := c.Quit()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "pcf: quit: %v\n", err)
-	}
-}
-
-// upload the file to the ftp server
-func put(f *os.File, n string, u *url.URL) {
-	if _, err := f.Seek(0, 0); err != nil {
-		fmt.Fprintf(os.Stderr, "pcf: failed to read: %v\n", err)
-		os.Exit(1)
-	}
-	c := login(u)
-	store(u, f, c, n)
-	exit(c)
-}
-
-// calculate and print the hash
-func hash(f *os.File) string {
-	if _, err := f.Seek(0, 0); err != nil {
-		fmt.Fprintf(os.Stderr, "pcf: failed to read: %v\n", err)
-		os.Exit(1)
-	}
-	h := sha1.New()
-	if _, err := io.Copy(h, f); err != nil {
-		fmt.Fprintf(os.Stderr, "pcf: failed to hash: %v\n", err)
-	}
-	return fmt.Sprintf("%x", h.Sum(nil))
-}
-
-func main() {
-	addr := os.Getenv("PCFSERVER")
-	if addr == "" {
-		fmt.Println("pcf: you must set the PCFSERVER environment variable!")
-		os.Exit(1)
-	}
-
-	// parse the url
-	u, err := url.Parse(addr)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "pcf: url configuration wrong: %v\n", err)
-	}
-	args := os.Args[1:]
-
-	if len(args) == 0 {
-		// use stdin data
-		put(os.Stdin, "file", u)
-		hash(os.Stdin)
-	} else {
-		// loop through and use all arguments
-		for _, arg := range args {
-			f, err := os.Open(arg)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "pcf: open: %v\n", err)
-				continue
-			}
-			defer f.Close()
-
-			// upload the file
-			put(f, filepath.Base(arg), u)
-
-			// calculate the hash
-			h := hash(f)
-
-			// print the url
-			u.Host = u.Hostname()
-			u.Path = h + filepath.Ext(arg)
-			fmt.Println(u)
-		}
 	}
 }
